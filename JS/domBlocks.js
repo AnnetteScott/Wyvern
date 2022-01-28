f183d6162360f15ec82744ae374a649a2bc74b8b
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-28 21:16:41 +1300

    Create domBlocks.js

---
+++ JS/domBlocks.js
@@ -0,0 +1,9 @@
+DOM_Blocks = {
+    small_project_card: function (number = 0, title = ""){
+        return '<div class="small_project_card">'
+        +          `<h1>${number}</h1>`
+        +          `<p>${title}</p>`
+        +          '<div class="menu"></div>'
+        +      '</div>';
+    }
+}

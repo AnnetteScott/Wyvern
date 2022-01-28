03fddc221e8c5358a17e7653b5d320dd63fdb352
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-29 11:33:37 +1300

    Update createProject.js
    - Delete confirm work
    - Prompt() is not support. Looking for alt

--- JS/createProject.js
+++ JS/createProject.js
@@ -8,25 +8,34 @@ project_template = function (){
     }
 }
 
-function createProject(){
+
+function createProject(){    
     let new_project = project_template();
-/* 
-    new_project.project_name = document.getElementById("pName").value;
-    new_project.project_duration = document.getElementById("duration").value;
-    new_project.project_start_date = document.getElementById("startDate").value;
-*/
-    projects.push(new_project);
 
-    console.log(projects); 
-
-    document.getElementById("projects_list").innerHTML += DOM_Blocks.small_project_card(projects.length, "Hats");
+    let pName = prompt("Please Enter A Project Name");
+    if(pName != null){
+        let duration = prompt("Please Enter The Total Number Of Weeks " + pName + " Will Run For");
+        if(/^\d+$/.test(duration)){
+            let startDate = prompt("Please Enter A Date In The Fromat dd/mm/yy e.g 03/01/22");
+            new_project.project_name = pName
+            new_project.project_duration = duration
+            new_project.project_start_date = startDate
+            projects.push(new_project);
+            document.getElementById("projects_list").innerHTML += DOM_Blocks.small_project_card(projects.length, new_project.project_name);
+        }
+        
+    }
+    else{
+        window.alert("Please Enter A Name");
+    }
 }
 
 function deleteAllProjects(){
-    alerty.title = "Warning";
+    /* alerty.title = "Warning";
     alerty.message = "You are about to delete all projects. Continue?";
-    alerty.options = ["Ok"]
-    alerty.show();
-    projects = [];
-    document.getElementById("projects_list").innerHTML = "";
+    alerty.options = ["Ok"] */
+    if (confirm("You are about to delete all projects. Continue?")) {
+        projects = [];
+        document.getElementById("projects_list").innerHTML = "";
+    }
 }

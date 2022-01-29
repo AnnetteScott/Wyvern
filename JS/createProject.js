b7a642ae8a05cb7d7a2bf247a354fcaf65f2f927
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-29 19:40:28 +1300

    fixed create project func

--- JS/createProject.js
+++ JS/createProject.js
@@ -1,10 +1,23 @@
-let projects = [];
+function createProject(){
+    let name = document.getElementById('project_name').value;
+    let keyname = name.replaceAll(" ", "_");
+    if(keyname in projects){
+        //Name already exists, throw error
+    }else if(createProjectValidate() === false){
+        document.querySelector('.creation_form').classList.add('form_error');
+    }else{
+        document.querySelector('.creation_form').classList.remove('form_error');
+        projects[keyname] = project_template();
+        let dur = parseInt(document.getElementById('project_duration').value);
+        projects[keyname]["project_duration"] = dur % 2 === 1 ? dur++ : dur; //Ensure project duration is even
+        projects[keyname]["project_start_date"] = document.getElementById('project_date').value;
 
-project_template = function (){
-    return {
-        project_name: '',
-        project_duration: 0,
-        project_start_date: null
+        //Clear inputs
+        document.getElementById('project_duration').value = "";
+        document.getElementById('project_date').value = "";
+        document.getElementById('project_name').value = "";
+        
+        changePage('PAGE_projects');
     }
 }
 

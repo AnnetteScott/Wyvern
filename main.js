f51467c6d33c27a12e29867292cd774f250bc830
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-29 14:35:32 +1300

    Update main.js

--- main.js
+++ main.js
@@ -1,78 +1,65 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow, Menu } = require('electron')
-const path = require('path')
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
 
-const createWindow = () => {
-  	// Create the browser window.
-	const mainWindow = new BrowserWindow({
-		width: 1200,
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+    // Create new window
+    mainWindow = new BrowserWindow({
+        width: 1200,
 		height: 800,
-		icon: __dirname + '/WyvernIcon.ico',
-	})
+		icon: __dirname + '/WyvernIcon.ico'
+    });
+    // Load html in window
+    mainWindow.loadFile('main.html');
+    // Quit app when closed
+    mainWindow.on('closed', function(){
+      app.quit();
+    });
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+  
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
+});
 
-    // and load the index.html of the app.
-  mainWindow.loadFile('main.html')
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  var menu = Menu.buildFromTemplate([
-        {
-            label: "File",
-            submenu: [
-                {
-                    label: 'Open', 
-                    accelerator: process.platform === 'darwin' ? 'Ctrl+O' : 'Ctrl+O',
-                    //click(){ app.quit() }
-                }, 
-                {
-                    label: 'Save', 
-                    accelerator: process.platform === 'darwin' ? 'Ctrl+S' : 'Ctrl+S',
-                    //click(){ app.quit() }
-                }, 
-                {
-                    label: 'New Timesheet', 
-                    accelerator: process.platform === 'darwin' ? 'Ctrl+T' : 'Ctrl+T',
-                    //click(){ app.quit() }
-                }, 
-                {
-                    label: 'Print', 
-                    accelerator: process.platform === 'darwin' ? 'Ctrl+P' : 'Ctrl+P',
-                    //click(){ app.quit() }
-                },
-                {
-                    label: 'Exit', 
-                    accelerator: process.platform === 'darwin' ? 'Ctrl+W' : 'Ctrl+W',
-                    click(){ app.quit() }
-                } 
-            ]
-        }
-    ])
-
-    Menu.setApplicationMenu(menu);
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-const electron = require('electron')
+// Create menu template
+const mainMenuTemplate =  [
+    // Each object is a dropdown
+    {
+        label: "File",
+        submenu: [
+            {
+                label: 'Open', 
+                accelerator: process.platform === 'darwin' ? 'Ctrl+O' : 'Ctrl+O',
+                //click(){ app.quit() }
+            }, 
+            {
+                label: 'Save', 
+                accelerator: process.platform === 'darwin' ? 'Ctrl+S' : 'Ctrl+S',
+                //click(){ app.quit() }
+            },  
+            {
+                label: 'Print', 
+                accelerator: process.platform === 'darwin' ? 'Ctrl+P' : 'Ctrl+P',
+                //click(){ app.quit() }
+            },
+            {
+                label: 'Exit', 
+                accelerator: process.platform === 'darwin' ? 'Ctrl+W' : 'Ctrl+W',
+                click(){ app.quit() }
+            } 
+        ]
+      
+    }
+];
 
 // Enable live reload for all the files inside your project directory
 require('electron-reload')(__dirname);
+
+  

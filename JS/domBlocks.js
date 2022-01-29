e5ddca9340c69d6ee6b9ca2a7a679b742d3d8c0b
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-29 19:42:00 +1300

    added clickability

--- JS/domBlocks.js
+++ JS/domBlocks.js
@@ -1,6 +1,6 @@
 DOM_Blocks = {
     small_project_card: function (number = 0, title = ""){
-        return '<div class="small_project_card">'
+        return '<div class="small_project_card" onclick="clickProject(event)">'
         +          `<h1>${number}</h1>`
         +          `<p>${title}</p>`
         +          '<div class="menu"></div>'

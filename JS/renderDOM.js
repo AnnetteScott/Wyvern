4da01f3bd25ea2735b97963e2f04ebd9a45ab0ad
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-29 19:44:33 +1300

    Added create page

--- JS/renderDOM.js
+++ JS/renderDOM.js
@@ -13,13 +13,13 @@ let renderDOM = {
 
     tasks: {
         t_projects: function (){
-            if(projects !== renderDOM.cache.c_projects){
+            if(projects != renderDOM.cache.c_projects){
                 let elem = '';
                 Object.keys(projects).forEach(function(k, i){
                     elem += DOM_Blocks.small_project_card(i, k.toString().replaceAll("_", " "));
                 });
                 document.getElementById("projects_list").innerHTML = elem;
-                renderDOM.cache.c_projects = projects;
+                renderDOM.cache.c_projects = { ...projects };
             }
         }
 

790665ed9e9203ab65b0467d5dc5e0ac7b077774
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-29 20:09:26 +1300

    Update renderDOM.js

--- JS/renderDOM.js
+++ JS/renderDOM.js
@@ -1,31 +1,52 @@
-let renderDOM = {
+let TaskManager = {
     enabled: true,
 
     start: function (){
-        if(renderDOM.enabled){window.requestAnimationFrame(renderDOM.render_loop);}
+        if(TaskManager.enabled){window.requestAnimationFrame(TaskManager.render_loop);}
     },
 
     render_loop: function (){
-        renderDOM.tasks.t_projects();
+        TaskManager.tasks.t_projects();
 
-        if(renderDOM.enabled){window.requestAnimationFrame(renderDOM.render_loop);}
+        if(TaskManager.enabled){window.requestAnimationFrame(TaskManager.render_loop);}
     },
 
     tasks: {
         t_projects: function (){
-            if(projects != renderDOM.cache.c_projects){
+            if(shallowEqual(projects, TaskManager.cache.c_projects) == false){
                 let elem = '';
                 Object.keys(projects).forEach(function(k, i){
                     elem += DOM_Blocks.small_project_card(i, k.toString().replaceAll("_", " "));
                 });
                 document.getElementById("projects_list").innerHTML = elem;
-                renderDOM.cache.c_projects = { ...projects };
+                TaskManager.cache.c_projects = { ...projects };
+            }
+        },
+
+        t_navigation: function (){
+            if(currentPage === TaskManager.cache.c_navigation){
+
             }
         }
-
     },
 
     cache: {
-        c_projects: null
+        c_projects: {},
+        c_navigation: null
     }
 }
+
+
+function shallowEqual(object1, object2){
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+    if(keys1.length !== keys2.length){
+        return false;
+    }
+    for(let key of keys1){
+        if(object1[key] !== object2[key]){
+            return false;
+        }
+    }
+    return true;
+  }

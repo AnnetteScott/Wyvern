79392ad63e96221e4c95230be07b3b4f17162517
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-29 19:40:41 +1300

    Check if project input is valid

--- JS/createProject.js
+++ JS/createProject.js
@@ -21,31 +21,29 @@ function createProject(){
     }
 }
 
-
-function createProject(){    
-    let new_project = project_template();
-
-    let pName = prompt("Please Enter A Project Name");
-    if(pName != null){
-        let duration = prompt("Please Enter The Total Number Of Weeks " + pName + " Will Run For");
-        if(/^\d+$/.test(duration)){
-            let startDate = prompt("Please Enter A Date In The Fromat dd/mm/yy e.g 03/01/22");
-            new_project.project_name = pName
-            new_project.project_duration = duration
-            new_project.project_start_date = startDate
-            projects.push(new_project);
-            document.getElementById("projects_list").innerHTML += DOM_Blocks.small_project_card(projects.length, new_project.project_name);
-        }
-        
+function createProjectValidate(){
+    if(document.getElementById('project_duration').value == "" || document.getElementById('project_duration').value == null){
+        return false;
+    }else if(document.getElementById('project_date').value == "" || document.getElementById('project_date').value == null){
+        return false;
+    }else if(document.getElementById('project_name').value == "" || document.getElementById('project_name').value == null){
+        return false;
+    } else {
+        return true;
     }
-    else{
-        window.alert("Please Enter A Name");
-    }
+    
 }
 
 function deleteAllProjects(){
     if (confirm("You are about to delete all projects. Continue?")) {
-        projects = [];
+        projects = {};
         document.getElementById("projects_list").innerHTML = "";
     }
+}
+
+function clickProject(e){
+    console.log("DA BEEB IS THE SMRTEST");
+    let project = e.target;
+    changePage('PAGE_project');
+
 }

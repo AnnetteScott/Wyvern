9ac048c9e00179c4b2396f71a9ca77379ca1a62c
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-01-29 19:40:03 +1300

    Create app.js

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+const electron = require('electron');
+const ipcRenderer = electron.ipcRenderer;
+ipcRenderer.on('save_Data', function (event) {
+    save_Data();
+});
+ipcRenderer.on('read_Data', function (event) {
+    read_Data();
+});
+
+let projects = {};
+read_Data();
+
+
+//This must be last
+renderDOM.start();

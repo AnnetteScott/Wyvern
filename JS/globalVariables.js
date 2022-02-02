e41560df5a00c7b428c8a753615835fa446d2b69
NotNatural21 <annettemorganscott@outlook.co.nz>
2022-02-02 21:58:03 +1300

    moved

--- JS/globalVariables.js
+++ JS/globalVariables.js
@@ -1,7 +1,9 @@
 let projectMasterDict = {};
+let colourMasterDict = {};
 let currentPage = 'wyvern';
 let currentChosenProject = '';
 let currentTimeSheet = '';
 let cellIsActive = false;
 let selectedCellsList = [];
 let colourCount = document.getElementsByClassName("color_button").length;
+

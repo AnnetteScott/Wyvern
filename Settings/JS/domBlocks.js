7673bcf682ace015ba976685f570b85bec92e0ff
NotNatural21 <annettemorganscott@outlook.co.nz>
2022-02-02 21:59:17 +1300

    Create domBlocks.js

---
+++ Settings/JS/domBlocks.js
@@ -0,0 +1,15 @@
+let DOM_Blocks_Settings = {
+    colour_card: function (colourName, color){
+        return '<div class="colour_card">'
+        +          `<div style="background-color: ${color}"></div>`
+        +          `<p>${colourName}</p>`
+        +      '</div>';
+
+    },
+    checkboxes: function (project, colourName){
+        return '<div class="check_box">'
+        +          `<input title="Project: ${project}" type="checkbox" id="settings_colour_${project}_${colourName}" name="accept" value="yes">`
+        +      '</div>';
+
+    }
+}

967b872b61d56127bfa5d866d78825d68fd9c862
NotNatural21 <annettemorganscott@outlook.co.nz>
2022-02-02 21:58:23 +1300

    Update domBlocks.js

--- Projects/JS/domBlocks.js
+++ Projects/JS/domBlocks.js
@@ -1,4 +1,4 @@
-DOM_Blocks = {
+let DOM_Blocks = {
     small_project_card: function (number = 0, title = ""){
         return '<div class="small_project_card" onclick="clickProject(event)">'
         +          `<h1>${number}</h1>`

c70be2a1c9e36854b3a3c48b34e3580571993471
NotNatural21 <annettemorganscott@outlook.co.nz>
2022-02-03 11:20:58 +1300

    Added Dynamic colour and colour selection

--- Settings/JS/colourHandler.js
+++ Settings/JS/colourHandler.js
@@ -1,11 +1,20 @@
-
-function checkColourStatus(){
-    if(colourMasterDict != {}){
-        Object.keys(colourMasterDict).forEach(function(colour){
-            Object.keys(projectMasterDict).forEach(function(proj){
-                let checkStatus = document.getElementById(`settings_colour_${proj}_${colour}`).checked;
-                console.log(checkStatus);
-            });
-        });
+function addToDict(e){
+    let checkBoxTitle = e.target.getAttribute('title').substr(9);
+    let checkBoxColourName = e.target.getAttribute('value');
+    let colourDict = colourMasterDict[checkBoxColourName][1];
+    if(e.target.checked){
+        if(colourDict['yes'].includes(checkBoxTitle)){       
+        } else {
+            const index = colourDict['no'].indexOf(checkBoxTitle);
+            if(index > -1){colourDict['no'].splice(index, 1)};
+            colourDict['yes'].push(checkBoxTitle)
+        }
+    } else if(e.target.checked === false){
+        if(colourDict['no'].includes(checkBoxTitle)){       
+        } else {
+            const index = colourDict['yes'].indexOf(checkBoxTitle);
+            if(index > -1){colourDict['yes'].splice(index, 1)};
+            colourDict['no'].push(checkBoxTitle)
+        }
     }
 }

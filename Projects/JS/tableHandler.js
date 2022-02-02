f4a36a473339a6f3c0bd7945588d315927878132
NotNatural21 <annettemorganscott@outlook.co.nz>
2022-02-02 16:27:49 +1300

    fixed cell selection bug

--- Projects/JS/tableHandler.js
+++ Projects/JS/tableHandler.js
@@ -6,7 +6,13 @@ function cellClicked(e){
     if(selectedCellsList != []){
         selectedCellsList.forEach(cellIDR => {
             let pCell = document.querySelector(`[value="${cellIDR}"]`);
-            cellDeSelect(pCell);
+            let colouredCells = projectMasterDict[currentChosenProject]["time_sheet_weeks"][currentTimeSheet]['cells'];
+            if(cellIDR in colouredCells){
+                cellDeSelect(pCell, colouredCells[cellIDR]);
+            } else {
+                cellDeSelect(pCell, "white");
+            }
+            
         });
     }
     selectedCellsList = [cellID];
@@ -30,21 +36,20 @@ function cellSelect(element){
     element.style.background = "#D1D3D9";
 }
 
-function cellDeSelect(element){
+function cellDeSelect(element, colour){
     element.style.borderColor = "black";
-    element.style.background = "white";
+    element.style.background = colour;
 }
 
 function setColour(e){
     let colourButtonStyle = window.getComputedStyle(e.target);
     let colour = colourButtonStyle.getPropertyValue('background-color');
     let projectWeekObj = projectMasterDict[currentChosenProject]["time_sheet_weeks"][currentTimeSheet];
-    console.log(colour);
     selectedCellsList.forEach(cellID => {
         let element = document.querySelector(`[value="${cellID}"]`);
         if(colour == "rgb(255, 255, 255)"){
             delete projectWeekObj['cells'][cellID];
-            cellDeSelect(element);
+            cellDeSelect(element, "white");
         } else{
             projectWeekObj['cells'][cellID] = colour;
             element.style.background = colour;
@@ -52,6 +57,7 @@ function setColour(e){
         }
         
     })
+
     selectedCellsList = [];
     
 }

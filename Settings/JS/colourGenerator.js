69ff03c6dfcd441a476535bea104b09ffa8a5cd0
NotNatural21 <annettemorganscott@outlook.co.nz>
2022-02-03 13:32:45 +1300

    fixed colour bug
    - colour was being stored in 2 different formats

--- Settings/JS/colourGenerator.js
+++ Settings/JS/colourGenerator.js
@@ -1,6 +1,7 @@
 function addColour(){
     let name = document.getElementById('colour_creation_name').value;
     let colour = document.getElementById('colour_creation_colour').value;
+    colour = hexToRgb(colour);
     if(name in colourMasterDict || name == '' || name == null){
         document.querySelector('.creation_form').classList.add('form_error');
     } else {
@@ -11,4 +12,15 @@ function addColour(){
         document.getElementById('colour_creation_name').value = ""; 
         changePage('PAGE_settings');
     }
+}
+
+function hexToRgb(c){
+    if(/^#([a-f0-9]{3}){1,2}$/.test(c)){
+        if(c.length== 4){
+            c= '#'+[c[1], c[1], c[2], c[2], c[3], c[3]].join('');
+        }
+        c= '0x'+c.substring(1);
+        return 'rgb('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+')';
+    }
+    return '';
 }

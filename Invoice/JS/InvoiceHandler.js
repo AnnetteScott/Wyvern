b6e01b65810b0d6caf31809c720c0f57478a8065
NotNatural21 <annettemorganscott@outlook.co.nz>
2022-02-04 20:49:14 +1300

    Created the ABILITY TO PRINT WITH CSS

--- Invoice/JS/InvoiceHandler.js
+++ Invoice/JS/InvoiceHandler.js
@@ -12,3 +12,29 @@ function createInvoice(){
     //changePage('PAGE_invoice');
     //document.querySelector("nav").classList.remove('hidden');
 }
+
+function printInvoice(elem = 'PRINTtheTHING'){
+    document.querySelector("nav").classList.add('hidden');
+    printDiv(elem);
+/*     let printingDoc = window.open();
+    printingDoc.document.write(document.getElementById(elem).innerHTML);
+    printingDoc.document.head.insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="Invoice/CSS/printout.css">`)
+    printingDoc.print();
+    printingDoc.close(); */
+    document.querySelector("nav").classList.remove('hidden');
+}
+
+function printDiv(id) {
+
+    let html = "";
+  
+    $('link').each(function() { // find all <link tags that have
+      if ($(this).attr('rel').indexOf('stylesheet') !=-1) { // rel="stylesheet"
+        html += '<link rel="stylesheet" href="'+$(this).attr("href")+'" />';
+      }
+    });
+    html += '<body onload="window.focus(); window.print()">'+$("#"+id).html()+'</body>';
+    let w = window.open("","print");
+    if (w) { w.document.write(html); w.document.close() }
+  
+  }
--- JS/fileIO.js
+++ JS/fileIO.js
@@ -17,21 +17,3 @@ function read_Data(){
     colourMasterDict = masterJSONRead['colourMasterDict'];
     userMasterDict = masterJSONRead['userMasterDict'];
 }
-
-function printInvoice(elem){
-    var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-
-    mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-    mywindow.document.write('</head><body >');
-    mywindow.document.write('<h1>' + document.title  + '</h1>');
-    mywindow.document.write(document.getElementById(elem).innerHTML);
-    mywindow.document.write('</body></html>');
-
-    mywindow.document.close(); // necessary for IE >= 10
-    mywindow.focus(); // necessary for IE >= 10*/
-
-    mywindow.print();
-    mywindow.close();
-
-    return true;
-}

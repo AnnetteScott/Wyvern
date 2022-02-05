a18a26e354ca5e93e2b1ba33879915c0ce3b992e
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-02-05 20:27:04 +1300

    fixed bug where user.json wouldn't be included in build

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ const mainMenuTemplate =  [
 ];
 
 // Enable live reload for all the files inside your project directory
-require('electron-reload')(__dirname);
+//require('electron-reload')(__dirname);
 
 
 function manual_save(){

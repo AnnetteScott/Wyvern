85723c70ef9929ca363da288eecd3110b9e8b8ce
NotNatural21 <annettemorganscott@outlook.co.nz>
2022-02-07 13:10:06 +1300

    finished settings

--- PROJECTS/projectGen.js
+++ PROJECTS/projectGen.js
@@ -1,51 +1,74 @@
+let selectedProjectID = '';
+
 function addNewProject(){
     let ClientID = $("#client_project_selection option:selected").val();
     let projectName = $("#create_project_name").val();
     let projectStartDate = reDoDate('create_project_date');
     let projectDuration = parseInt($("#create_project_duration").val());
 
-
     if(typeof ClientID == 'undefined'){
         $("#client_project_selection").addClass('form_error');
     }else if(projectName == ''){
         $("#create_project_name").addClass('form_error');
         $("#client_project_selection").removeClass('form_error');
-    }else if(projectStartDate == 'NaN/NaN/NaN'){
+    }else if(projectStartDate == 'NaN/NaN/NaN' && selectedProjectID == ''){
         $("#create_project_date").addClass('form_error');
         $("#create_project_name").removeClass('form_error');
-    }else if(projectStartDate == 'NaN'){
+    }else if(projectStartDate == 'NaN' && selectedProjectID == ''){
         $("#create_project_duration").addClass('form_error');
         $("#create_project_date").removeClass('form_error');
     }else{
-
-        //Generate unique projectID 
+ 		//Generate unique projectID 
         let projectID = generateID();
-        while(masterDict['projects'].hasOwnProperty(projectID)){
-            projectID = generateID();
-        }
-
-        masterDict['projects'][projectID] = {'projectName': projectName};
-        masterDict['projects'][projectID]['colourList'] = [];
-        masterDict['projects'][projectID]['weeks'] = {};
-
         //Make project duration even
         if(projectDuration % 2 == 1){
             projectDuration++;
         }
+        if(selectedProjectID == ''){
+            while(masterDict['projects'].hasOwnProperty(projectID)){
+                projectID = generateID();
+            }
+            masterDict['projects'][projectID] = {'projectName': projectName, 'projectStartDate': $("#create_project_date").val(), 'projectDuration': projectDuration};
 
-        let previousDate = projectStartDate;
-        for(let i = 1; i < projectDuration; i += 2){
-            let weekTwo = i + 1;
-            let weekTitle = i.toString() + " - " + weekTwo.toString();
-            
-            masterDict['projects'][projectID]['weeks'][weekTitle] = {'startDate': previousDate, 'colourCells': {}, 'totalColour': {}, 'totalCol': {}}
-            previousDate = addToDate(previousDate, 14)
+            masterDict['projects'][projectID]['colourList'] = [];
+            masterDict['projects'][projectID]['weeks'] = {};
+			let previousDate = projectStartDate;
+			for(let i = 1; i < projectDuration; i += 2){
+				let weekTwo = i + 1;
+				let weekTitle = i.toString() + " - " + weekTwo.toString();
+				
+				masterDict['projects'][projectID]['weeks'][weekTitle] = {'startDate': previousDate, 'colourCells': {}, 'totalColour': {}, 'totalCol': {}}
+				previousDate = addToDate(previousDate, 14)
+			}
+			
+        }else{
+			let previousClientID = '';
+			for (const [clientID, clientDict] of Object.entries(masterDict['clients'])) {
+				const indexProj = clientDict['projects'].indexOf(selectedProjectID);
+				if (indexProj > -1) {
+					previousClientID = clientID;
+				}
+			};
+            masterDict['projects'][selectedProjectID]['projectName'] = projectName;
+			const index = masterDict['clients'][previousClientID]['projects'].indexOf(selectedProjectID);
+			masterDict['clients'][previousClientID]['projects'].splice(index, 1);
+			projectID = selectedProjectID
         }
-
-        masterDict['clients'][ClientID]['projects'].push(projectID);
-
+		masterDict['clients'][ClientID]['projects'].push(projectID);
         $("#create_project_name").val('')
         $("#create_project_duration").val('')
         $('#project_pop_up').removeClass('input_box_open');
+		selectedID = '';
     }
+}
+
+function editProject(e){
+	let projectID = $(e.target).attr('projectid');
+    let projectDict = masterDict['projects'][projectID];
+    $("#create_project_name").val(projectDict['projectName']);
+	$("#create_project_date").css({"filter": "blur(2px)", "pointer-events": "none"})
+	$("#create_project_duration").css({"filter": "blur(2px)", "pointer-events": "none"})
+
+	$('#project_pop_up').addClass('input_box_open');
+	selectedProjectID = projectID;
 }

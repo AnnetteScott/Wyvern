7486841e3d0337d989cfcb7b143447eb2eaaeb21
NotNatural21 <annettemorganscott@outlook.co.nz>
2022-02-07 15:42:05 +1300

    Update updateAll.js

--- SETTINGS/updateAll.js
+++ SETTINGS/updateAll.js
@@ -16,12 +16,19 @@ function updateAll(){
 
 	//Project Updater
 	let projectElement = $("#project_list");
+	let projectElementProjects = $("#project_list_projects");
 	elem = '';
+	let projectElem = '';
 	for (const [projectID, projectDict] of Object.entries(masterDict['projects'])) {
 		elem += DOM_Blocks_Settings.project_card(projectDict['projectName'], projectID);
+		projectElem += DOM_Blocks_projects.project_card(projectDict['projectName'], projectID);
 	}
 	projectElement.empty();
+	projectElementProjects.empty();
 	projectElement.append(elem);
+	projectElementProjects.append(projectElem);
+
+
 	//place client options on project creation page
 	elem = ''
 	for (const [clientID, clientDict] of Object.entries(masterDict['clients'])) {

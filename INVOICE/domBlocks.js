2233294cc8100951f4a2fcceeb703909753869f8
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-03-16 11:24:39 +1300

    moving to vue

--- INVOICE/domBlocks.js
+++
@@ -1,5 +0,0 @@
-let DOM_Blocks_invoice = {
-    invoice_col: function (colID){
-        return `<div id="ts_col_${colID}" class="invoice_sheet_column"></div>`
-    }
-}
--- SETTINGS/checkLists.js
+++
@@ -1,5 +0,0 @@
-//Toggles open/closed the dropdown of a specified checklist.
-//Called from the click listener of a checklist element.
-function toggleChecklist(e){
-	$(e.target).parent().toggleClass('visible');
-}

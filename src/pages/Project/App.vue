32cc6d20e53bb265c570667e74f610be860399c4
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-03-22 08:57:45 +1300

    finished settings

---
+++ src/pages/Project/App.vue
@@ -0,0 +1,69 @@
+<template>
+	<BackgroundBubble />
+	<NavBar 
+		:title="`Project Time Sheets`"
+		:links="[
+			{
+				title: `Wyvern`,
+				image: require(`../../assets/icons/home_white_24dp.svg`),
+				url: `/`
+			},
+			{
+				title: `Settings`,
+				image: require(`../../assets/icons/settings_white_24dp.svg`),
+				url: `/Settings.html`
+			},
+			{
+				title: `Time Sheets`,
+				image: require(`../../assets/icons/schedule_white_24dp.svg`),
+				url: `/TimeSheets.html`
+			},
+			{
+				title: `Invoice`,
+				image: require(`../../assets/icons/receipt_white_24dp.svg`),
+				url: `/Invoice.html`
+			},
+			{
+				title: `Records`,
+				image: require(`../../assets/icons/receipt_long_white_24dp.svg`),
+				url: `/Records.html`
+			}
+		]"
+	/>    
+    
+
+</template>
+
+<script>
+import NavBar from '../../components/NavBar.vue';
+import BackgroundBubble from '../../components/BackgroundBubble.vue';
+
+export default {
+	name: 'App',
+	components: {
+		NavBar,
+		BackgroundBubble
+	},
+    data() {
+		return {
+			masterDict: {}
+		}
+	},
+	mounted() {
+		this.masterDict = JSON.parse(localStorage.getItem('masterDict'));
+	},
+}
+</script>
+
+<style>
+@import url('../../../public/root.css');
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: black;
+	margin-top: var(--navbar_height);
+}
+
+</style>

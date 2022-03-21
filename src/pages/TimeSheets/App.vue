3e63c7792b9cfddb9403faf21c3826099d560a9c
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-03-22 08:57:54 +1300

    finished settings

--- src/pages/TimeSheets/App.vue
+++ src/pages/TimeSheets/App.vue
@@ -30,6 +30,14 @@
 			}
 		]"
 	/>
+    <div id="inner">
+        <div id="project_container">
+            <a v-for="(projectDict, projectID) in masterDict['projects']" :key="projectDict" class="list_item" :data="projectID" :href="`/Project.html`">{{ projectDict.name }}</a> 
+        </div>
+    </div>
+    
+    
+
 </template>
 
 <script>
@@ -42,19 +50,71 @@ export default {
 		NavBar,
 		BackgroundBubble
 	},
+    data() {
+		return {
+			masterDict: {}
+		}
+	},
 	mounted() {
-		
-	}
+		this.masterDict = JSON.parse(localStorage.getItem('masterDict'));
+	},
 }
 </script>
 
 <style>
+@import url('../../../public/root.css');
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+	color: black;
+	margin-top: var(--navbar_height);
+}
+
+#inner{
+    width: 100%;
+    height: calc(100vh - var(--navbar_height));
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+#project_container {
+    width: 100%;
+    z-index: 2;
+	position: relative;
+	display: flex;
+	flex-flow: column nowrap;
+	justify-content: flex-start;
+	align-items: center;
+	gap: 10px;
+	min-height: calc(100vh - var(--navbar_height) - 110px);
+	overflow-y: scroll;
+	margin: 1px 10px 10px 10px;
+	box-shadow: 0px 0px 10px -5px white inset, 0px 4px 16px -16px black;
+}
+
+.list_item {
+	position: relative;
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: center;
+	align-items: center;
+	width: 95%;
+	height: clamp(60px, 60px, 60px);
+	padding: 0px 20px;
+	background-color: #ffffff40;
+	border-radius: 10px;
+	cursor: pointer;
+	font-family: 'Lora';
+	font-weight: 900;
+	font-size: 2.25em;
+    text-decoration: none;
+    color: black;
+}
+
+.list_item:hover {
+	box-shadow: 0 14px 28px rgba(0, 0, 0, 0.082), 0 10px 10px rgba(0, 0, 0, 0.11);
 }
 </style>

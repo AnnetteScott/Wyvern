24bb2febdf8f134ed1f05835d11763de0d924d20
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-03-21 21:02:35 +1300

    Update ButtonItem.vue

--- src/components/ButtonItem.vue
+++ src/components/ButtonItem.vue
@@ -1,34 +1,51 @@
 <template>
-	<button onclick="">{{ title }}</button>
+	<div class="button_link" :title="$props.title">
+		<p>{{ title }}</p>
+	</div>
 </template>
 
 <script>
 export default {
-	name: 'ButtonItem',
+	name: 'ButtonLink',
 	props: {
-		title: String
+		title: String,
+		glow: Boolean
 	}
 }
 </script>
 
 <style scoped>
-button, .button {
-	margin: 5px 0px;
-	padding: 5px 20px;
-	color: white;
-	font-family: 'Segoe UI', sans-serif;
+.button_link {
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: fit-content;
+	min-height: 20px;
+	max-height: 50px;
+	margin: 10px 0px 0px 0px;
+	font-family: inherit;
+	font-size: 17px;
+	font-weight: bold;
 	text-decoration: none;
-	background: linear-gradient(45deg, #db0c0c, #8d0505);
-	border: unset;
-	border-radius: 5px;
+	background: linear-gradient(90deg, #8202ccb2, #bb02ccb2);
+	box-shadow: 0px 15px 30px -10px #8202cc;
+	border-radius: 30px;
+	overflow: hidden;
 	cursor: pointer;
 }
-
-button:hover, .button:hover {
-	background: linear-gradient(45deg, #e400d8, #9700dd);
+.button_link p {
+	z-index: 1;
+	margin: -2px 10px 0px 10px;
+	padding: 2px 0px;
+	color: white;
+	font-family: inherit;
+	font-weight: 100;
+	text-decoration: none;
+	user-select: none;
 }
 
-.active_button {
-	background: linear-gradient(180deg, #0c77db, #990cdb);
+.button_link:hover{
+    background: linear-gradient(90deg, #8202cc, #bb02cc);
 }
 </style>
